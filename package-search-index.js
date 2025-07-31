e93ee707a206ce42dd7311742063a6a86bd05971
noblenumbat <noblenumbat@users.noreply.github.com>
2025-07-31 11:05:04 +0000

    deploy: caa95503143ef663b3a247b33e7344ee503a8e37

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"practices"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"poo"},{"l":"practices"}];updateSearchResults();
