f148697bbc485337db59bd6a790dc14a62a1a349
noblenumbat <noblenumbat@users.noreply.github.com>
2025-09-17 10:36:34 +0000

    deploy: 21bc2d39866aa2a20d0d00675d9de98de4e04cc0

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"poo"},{"l":"poo.equals_practices"},{"l":"practices"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"poo"},{"l":"poo.equals_practices"},{"l":"poo.exceptions"},{"l":"practices"}];updateSearchResults();
